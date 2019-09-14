c04b6b640ad5c06458931122918967965f670722
Anupriyam <anupriyamranjit8@gmail.com>
2019-09-14 17:09:32 -0400

    Added about

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import UploadImage from './components/UploadImage'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import 'react-bootstrap';
+import About from './components/About'
 class App extends Component {
 
   render() {
     return (
       <div>
         <Header />
+        <About />
         <UploadImage />
       </div>
     );
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
-export default function Header() {
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+export default function About () {
   return (
-    <header style ={headerStyle}>
-      
-    </header>
+
+ <Jumbotron>
+   <h1>Hello, world!</h1>
+   <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+   </p>
+   <p>
+    <Button variant="primary">Learn more</Button>
+   </p>
+ </Jumbotron>
   )
 }
 
