f9b49abb0d57c940c610c446432823fe18003167
Anupriyam <anupriyamranjit8@gmail.com>
2019-09-14 16:49:07 -0400

    Added Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import UploadImage from './components/UploadImage'
 import './App.css';
-
+import 'react-bootstrap';
 class App extends Component {
 
   render() {
---
+++ src/components/About.js
@@ -0,0 +1,80 @@
+Last login: Sun Sep  8 11:25:24 on ttys001
+ls
+l%                                                                                      (base) ➜  ~ ls
+AndroidStudioProjects Library               django_first          nltk_data
+Applications          Movies                docker_files          package-lock.json
+Desktop               Music                 flutter
+Documents             Pictures              module_5_auto.csv
+Downloads             Public                my_first_app
+(base) ➜  ~ ls
+AndroidStudioProjects Library               django_first          nltk_data
+Applications          Movies                docker_files          package-lock.json
+Desktop               Music                 flutter
+Documents             Pictures              module_5_auto.csv
+Downloads             Public                my_first_app
+(base) ➜  ~ cd Desktop/ML-Diabetes
+(base) ➜  ML-Diabetes git:(master) ✗ ls
+README.md         package-lock.json public
+node_modules      package.json      src
+(base) ➜  ML-Diabetes git:(master) ✗ cd src
+(base) ➜  src git:(master) ✗ ls
+App.css          App.test.js      index.css        logo.svg
+App.js           components       index.js         serviceWorker.js
+(base) ➜  src git:(master) ✗ cd components
+(base) ➜  components git:(master) ✗ ls
+Header.js      UploadImage.js
+(base) ➜  components git:(master) ✗ cd ..
+(base) ➜  src git:(master) ✗ cd
+(base) ➜  ~ ccd Desktop/ML-Diabetes
+zsh: command not found: ccd
+(base) ➜  ~ ls
+AndroidStudioProjects Library               django_first          nltk_data
+Applications          Movies                docker_files          package-lock.json
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header style ={headerStyle}>
+      <h1>Toe Sucker</h1>
+    </header>
+  )
+}
+
+const headerStyle = {
+    background: '#8053c9',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '10px'
+  }
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"Header.js" [noeol] 16L, 270C
