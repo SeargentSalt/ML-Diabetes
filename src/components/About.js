78ddf5d02491bbab88f00b9492326b384f63f19b
Anupriyam <anupriyamranjit8@gmail.com>
2019-09-14 16:54:58 -0400

    Changed About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,35 +1,3 @@
-Last login: Sun Sep  8 11:25:24 on ttys001
-ls
-l%                                                                                      (base) ➜  ~ ls
-AndroidStudioProjects Library               django_first          nltk_data
-Applications          Movies                docker_files          package-lock.json
-Desktop               Music                 flutter
-Documents             Pictures              module_5_auto.csv
-Downloads             Public                my_first_app
-(base) ➜  ~ ls
-AndroidStudioProjects Library               django_first          nltk_data
-Applications          Movies                docker_files          package-lock.json
-Desktop               Music                 flutter
-Documents             Pictures              module_5_auto.csv
-Downloads             Public                my_first_app
-(base) ➜  ~ cd Desktop/ML-Diabetes
-(base) ➜  ML-Diabetes git:(master) ✗ ls
-README.md         package-lock.json public
-node_modules      package.json      src
-(base) ➜  ML-Diabetes git:(master) ✗ cd src
-(base) ➜  src git:(master) ✗ ls
-App.css          App.test.js      index.css        logo.svg
-App.js           components       index.js         serviceWorker.js
-(base) ➜  src git:(master) ✗ cd components
-(base) ➜  components git:(master) ✗ ls
-Header.js      UploadImage.js
-(base) ➜  components git:(master) ✗ cd ..
-(base) ➜  src git:(master) ✗ cd
-(base) ➜  ~ ccd Desktop/ML-Diabetes
-zsh: command not found: ccd
-(base) ➜  ~ ls
-AndroidStudioProjects Library               django_first          nltk_data
-Applications          Movies                docker_files          package-lock.json
 import React from 'react'
 
 export default function Header() {
