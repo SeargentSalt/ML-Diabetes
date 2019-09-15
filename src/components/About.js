ee3e55d6b97bd0a2400abc872bb0dc67cdec98e3
Anupriyam <anupriyamranjit8@gmail.com>
2019-09-15 01:26:07 -0400

    Update

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,9 @@ export default function About () {
  <Jumbotron>
    <h1>What is Diabetic Retinopathy </h1>
    <p>Millions of people suffer from diabetic retinopathy, the leading cause of blindness among working aged adults. Aravind Eye Hospital in India hopes to detect and prevent this disease among people living in rural areas where medical screening is difficult to conduct.  </p>
-   <p>
-    <Button variant="primary">Learn more</Button>
+
+<p>
+    <Button href="https://www.mayoclinic.org/diseases-conditions/diabetic-retinopathy/symptoms-causes/syc-20371611" variant="primary">Learn more</Button>
    </p>
  </Jumbotron>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,8 @@ export default function NavigationBar () {
 <Navbar bg="primary" variant="dark">
     <Navbar.Brand href="#About">Navbar</Navbar.Brand>
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#UploadImage">Home</Nav.Link>
       <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
     <Form inline>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" />
