f1ea1641380313e196dec811f8eba55587aee621
Anupriyam <anupriyamranjit8@gmail.com>
2019-09-14 20:52:27 -0400

    added stuff

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import 'react-bootstrap';
+import NavigationBar from './components/Navbar';
 import About from './components/About'
 class App extends Component {
 
   render() {
     return (
       <div>
+	<NavigationBar />
         <Header />
         <About />
         <UploadImage />
--- src/components/About.js
+++ src/components/About.js
@@ -5,21 +5,11 @@ export default function About () {
   return (
 
  <Jumbotron>
-   <h1>Hello, world!</h1>
-   <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-   </p>
+   <h1>What is Diabetic Retinopathy </h1>
+   <p>Millions of people suffer from diabetic retinopathy, the leading cause of blindness among working aged adults. Aravind Eye Hospital in India hopes to detect and prevent this disease among people living in rural areas where medical screening is difficult to conduct.  </p>
    <p>
     <Button variant="primary">Learn more</Button>
    </p>
  </Jumbotron>
   )
 }
-
-const headerStyle = {
-    background: '#8053c9',
-    color: '#fff',
-    textAlign: 'center',
-    padding: '10px'
-  }
